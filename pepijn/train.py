f613dc917758058b70ee913e867e42695995224f
pvdklei <pepijnvanderklei@gmail.com>
2021-01-20 01:12:37 +0100

    training class

--- pepijn/train.py
+++ pepijn/train.py
@@ -11,161 +11,23 @@ import numpy as np
 import time
 import utils
 
-def train(model, 
-          optimizer, 
-          trainloader, 
-          testloader, 
-          criterion=nn.CrossEntropyLoss(), 
-          epochs=5, 
-          loaders=None):
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    print("Training on: ", device)
-
-    train_losses, test_losses = [], []
-
-    start_time = time.time()
-
-    for epoch in range(epochs):
-
-        # validation
-        testloss = []
-        for images, labels in testloader:
-            images, labels = images.to(device), labels.to(device)
-            with torch.no_grad():
-                out = model(images)
-                loss = criterion(out, labels)
-                testloss.append(loss.item())
-        testloss = np.mean(testloss)
-        print(f"Epoch {epoch}, Validation loss: {testloss}")
-        test_losses.append(testloss)
-
-
-        # training
-        trainloss = []
-        for images, labels in trainloader:
-            images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()
-            out = model(images)
-            loss = criterion(out, labels)
-            loss.backward()
-            optimizer.step()
-            trainloss.append(loss.item())
-        trainloss = np.mean(trainloss)
-        print(f"Epoch {epoch}, Training loss: {trainloss}")
-        train_losses.append(trainloss)
-
-        if loaders:
-            trainloader, testloader = loaders()
-
-    end_time = time.time()
-
-    # final accuracy
-    totacc = 0
-    n = 0
-    for images, labels in testloader:
-        images, labels = images.to(device), labels.to(device)
-        with torch.no_grad():
-            out = model(images)
-            topv, topi = torch.topk(out, 1, dim=1)
-            labels.resize_(*topi.shape)
-            eq = topi == labels
-            acc = torch.mean(eq.type(torch.FloatTensor))
-            totacc += acc.item()
-            n += 1
-    totacc /= n
-    print(f"The final total accuracy is: {totacc * 100}")
-    return train_losses, test_losses, totacc, end_time - start_time
-
-
-def autotrain(model, optimizer, trainloader, testloader, criterion=nn.CrossEntropyLoss(), epochs=5):
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    print(device)
-
-    lowest_testloss = np.Inf
-
-    for epoch in range(epochs):
-
-        # training
-        trainloss = 0
-        for images, labels in trainloader:
-            images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()
-            out = model(images)
-            loss = criterion(out, images)
-            loss.backward()
-            optimizer.step()
-            trainloss += loss.item()
-        print(f"Epoch {epoch}, Training loss: {trainloss}")
-
-        # validation
-        testloss = 0
-        for images, labels in testloader:
-            images, labels = images.to(device), labels.to(device)
-            with torch.no_grad():
-                out = model(images)
-                loss = criterion(out, images)
-                testloss += loss
-        print(f"Epoch {epoch}, Validation loss: {testloss}")
-        lowest_testloss = testloss
-
-def training_eval(save_as="evaluation", 
-                  descr="An evaluation of the training of a model",
-                  **train_params):
-
-    if "optimizer" not in train_params.keys():
-        train_params["optimizer"] = optim.Adam(params=train_params["model"].parameters(), lr=0.001)
-
-    train_losses, test_losses, accuracy, time = train(**train_params)
-    stats = dict(train_losses=train_losses,
-                 test_losses=test_losses,
-                 accuracy=accuracy,
-                 description=decr,
-                 time=time)
-
-    utils.save_pickle(stats, save_as)
-
-def show_training_eval(path):
-    data = utils.load_pickle(path)
-    path = path.split("/")[-1]
-    print(f"Evaluation of {path}\n")
-    if "description" in data.keys():
-        print(f"Description:\n\n{data['description']}\n")
-    print(f"The final accuracy after training was {round(data['accuracy'], 1)}")
-    print(f"Training took {round(data['time'], 1)} seconds\n")
-    x = range(len(data["train_losses"]))
-    plt.plot(x, data["train_losses"], label="Training Loss")
-    plt.plot(x, data["test_losses"], label="Evaluation Loss")
-    plt.legend()
-    plt.show()
-
-def compare_training_evals(*paths):
-    for path in paths:
-        data = utils.load_pickle(path)
-        name = path.split("/")[-1]
-        x = range(len(data["test_losses"]))
-        acc = round(data['accuracy']*100, 1)
-        time = round(data["time"], 1)
-        label = f"{name} ({acc}% in {time}s"
-        plt.plot(x, data["test_losses"], label=label)
-    plt.legend()
-    plt.show()
-
-
-class TrainingStats:
-    def __init__(self, train_losses, test_losses, accuracy, time, title="", descr="", model=None):
+class Training:
+    def __init__(self, 
+                 autotrain=False,
+                 train_losses=[], 
+                 test_losses=[], 
+                 accuracy=0.0, 
+                 time=0.0, 
+                 title="", 
+                 descr=""):
         self.train_losses = train_losses
         self.test_losses = test_losses
         self.time = time
         self.title = title
         self.description = descr
         self.accuracy = accuracy
-        self.model = model
+        self.autotrain = autotrain
     
     @classmethod
     def from_dict(cls, data: dict):
@@ -176,12 +38,29 @@ class TrainingStats:
                    descr=data["description"])
 
     @classmethod
-    def from_tuple(cls, data: tuple):
-        return cls(train_losses=data[0],
-                   test_losses=data[1],
-                   time=data[2],
-                   accuracy=data[3])
+    def load(cls, path):
+        data = utils.load_pickle(path)
+        if type(data) == dict:
+            return cls.from_dict(data)
+        elif type(data) == cls:
+            return data
+        else:
+            raise Exception("Not a valid training stats format found in pickle")
+
+    def save(self, path):
+        utils.save_pickle(self, path)
  
+    @staticmethod
+    def compare(*trainings):
+        for training in trainings:
+            x = range(len(training.test_losses))
+            acc = round(training.accuracy*100, 1)
+            time = round(training.time, 1)
+            label = f"{training.title} ({acc}% in {time}s"
+            plt.plot(x, training.testlosses, label=label)
+        plt.legend()
+        plt.show()
+    
 
     def summary(self):
         print(f"Title: {self.title}")
@@ -200,3 +79,89 @@ class TrainingStats:
         self.test_losses.extend(test_losses)
         self.time += time
         self.accuracy = accuracy
+
+    def train(self, 
+              model, 
+              trainloader, 
+              testloader, 
+              optimizer=None,
+              criterion=nn.CrossEntropyLoss(), 
+              epochs=5, 
+              loaders=None):
+    
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+    
+        if optimizer == None:
+            optimizer = optim.Adam(params=model.parameters(), lr=0.001)
+    
+    
+        print("Training on: ", device)
+    
+        train_losses, test_losses = [], []
+    
+        start_time = time.time()
+    
+        for epoch in range(epochs):
+    
+            # validation
+            testloss = []
+            for images, labels in testloader:
+                images, labels = images.to(device), labels.to(device)
+                with torch.no_grad():
+                    out = model(images)
+                    loss = criterion(out, labels)
+                    testloss.append(loss.item())
+            testloss = np.mean(testloss)
+            print(f"Epoch {epoch}, Validation loss: {round(testloss, 3)}")
+            test_losses.append(testloss)
+    
+    
+            # training
+            trainloss = []
+            for images, labels in trainloader:
+                images, labels = images.to(device), labels.to(device)
+                optimizer.zero_grad()
+                out = model(images)
+                if self.autotrain:
+                    loss = criterion(out, images)
+                else:
+                    loss = criterion(out, labels)
+                loss.backward()
+                optimizer.step()
+                trainloss.append(loss.item())
+            trainloss = np.mean(trainloss)
+            print(f"Epoch {epoch}, Training loss: {round(trainloss, 3)}")
+            train_losses.append(trainloss)
+    
+            if loaders:
+                trainloader, testloader = loaders()
+    
+        end_time = time.time()
+    
+        # final accuracy
+        totacc = 0
+        n = 0
+        for images, labels in testloader:
+            images, labels = images.to(device), labels.to(device)
+            with torch.no_grad():
+                out = model(images)
+                topv, topi = torch.topk(out, 1, dim=1)
+                labels.resize_(*topi.shape)
+                eq = topi == labels
+                acc = torch.mean(eq.type(torch.FloatTensor))
+                totacc += acc.item()
+                n += 1
+        totacc /= n
+        print(f"The final total accuracy is: {totacc * 100}")
+        self.add(train_losses, test_losses, totacc, end_time - start_time)
+
+
+
+
+
+
+
+
+
+
